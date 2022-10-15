3155a0612163c18b1b3f6b0467c0908867b44847
Ekemwhite <ekemwhite@gmail.com>
2022-10-15 00:20:17 -0700

    task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,12 +3,15 @@
  * main - prints all single digit numbers of base 10 starting from0
  * Return: Always 0
  */
+
 int main(void)
 {
 	int digit;
-	for (digit = 0, digit < 10, digit++)
-		putchar((digit % 10) + "0");
 
-	putchar("\n");
+	for (digit = 0; digit < 10; digit++)
+		putchar((digit % 10) + '0');
+
+	putchar('\n');
+
 	return (0);
 }
