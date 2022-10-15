aad22918dd6aa833887ef9aee2d4ffa012d9086c
Ekemwhite <ekemwhite@gmail.com>
2022-10-15 00:49:24 -0700

    task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,7 +9,7 @@ int main(void)
 	char alphabet;
 
 	for (digit = '0'; digit < '10'; digit++)
-		putchar((digit % 10) + "0");
+		putchar((digit % 10) + '0');
 	
 	for (alphabet = 'a'; alphabet <= 'f'; alphabet++)
 		putchar(alpabet);
