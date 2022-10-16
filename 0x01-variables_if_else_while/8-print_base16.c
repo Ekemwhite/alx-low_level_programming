e35660dfc426145133da9b863c88bfc07f7223e3
Ekemwhite <ekemwhite@gmail.com>
2022-10-15 19:34:07 -0700

    taask8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,12 +8,13 @@ int main(void)
 	int digit;
 	char alphabet;
 
-	for (digit = '0'; digit < '10'; digit++)
+	for (digit = 0; digit < 10; digit++)
 		putchar((digit % 10) + '0');
-	
+
 	for (alphabet = 'a'; alphabet <= 'f'; alphabet++)
 		putchar(alpabet);
 	
-	putchar("\n");
+	putchar('\n');
+
 	return (0);
 }
