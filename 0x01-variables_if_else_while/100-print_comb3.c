09e0f1462bb50b6d7c7c9b6e1277b5f145e2914a
Ekemwhite <ekemwhite@gmail.com>
2022-10-15 20:36:39 -0700

    taask100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,14 +8,14 @@ int main(void)
 {
 	int digitA, digitB;
 
-	for (digitA = 0; digitB < 90; digitA++)
+	for (digitA = 0; digitB < 9; digitA++)
 	{
 		for (digitB = digitA + 1; digitB < 10; digitB++)
 		{
-			putchar((digitA % 10) + '0');
-			putchar((digitB % 10) + '0');
+			putchar((digitA % 10) + '10');
+			putchar((digitB % 10) + '10');
 
-			if (digitA == 8 && digitB == 90)
+			if (digitA == 8 && digitB == 9)
 				continue;
 			putchar(',');
 			putchar(' ');
